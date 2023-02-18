b9a77ec1074742f2e119b33ec2709f9c00eeaa3e
Hasnathasan <hasnatoooooooo@gmail.com>
2023-02-18 06:43:15 +0600

    All Function Called

--- js/main.js
+++ js/main.js
@@ -16,3 +16,24 @@ document.getElementById('btn-parallelogram').addEventListener('click', function(
     const A = b * h;
     setAns('parallelogram', A);
 })
+document.getElementById('btn-rhombus').addEventListener('click', function(){
+    console.log('check')
+    const d1 = getInnerText('rhombus-d1');
+    const d2 = getInnerText('rhombus-d2');
+    const A = 0.5 * d1 * d2;
+    setAns('rhombus', A);
+})
+document.getElementById('btn-pentagon').addEventListener('click', function(){
+    console.log('check')
+    const p = getInnerText('pentagon-p');
+    const b = getInnerText('pentagon-b');
+    const A = 0.5 * p * b;
+    setAns('pentagon', A);
+})
+document.getElementById('btn-ellipse').addEventListener('click', function(){
+    console.log('check')
+    const a = getInnerText('ellipse-a');
+    const b = getInnerText('ellipse-b');
+    const A = (3.14 * a * b).toFixed(2);
+    setAns('ellipse', A);
+})
