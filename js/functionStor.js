9a328057a409efb906c00267198337d8ba6a17d0
Hasnathasan <hasnatoooooooo@gmail.com>
2024-01-07 23:53:58 +0600

    Code formated of js files

--- js/functionStor.js
+++ js/functionStor.js
@@ -1,18 +1,18 @@
 function getInnerText(id) {
-    const innerText = document.getElementById(id).innerText;
-    return innerText;
+  const innerText = document.getElementById(id).innerText;
+  return innerText;
 }
-function getValueFromInput(id){
+function getValueFromInput(id) {
   const value = document.getElementById(id).value;
   return value;
 }
 
 
 
-function setAns(name, A){
+function setAns(name, A) {
   const div = document.createElement('div');
-  div.innerHTML = 
-  `<li>${name}</li>
+  div.innerHTML =
+    `<li>${name}</li>
   <li>${A} cm<sup>2</sup></li>
   <button class="btn btn-primary fs-6">Convert to m<sup>2</sup></button>
   `;
@@ -25,25 +25,25 @@ function setAns(name, A){
 
 
 function getRandomColor() {
-  const  letters = '0123456789abcdef';
+  const letters = '0123456789abcdef';
   let color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
+  return color;
+}
 
 
 
 
-  function setValue(input1, input2, set1, set2){
-    const value1 = document.getElementById(input1).value;
-    const setaddress1 = document.getElementById(set1);
-    setaddress1.innerText = value1;
-    const value2 = document.getElementById(input2).value;
-    const setAddress2 = document.getElementById(set2)
-    setAddress2.innerText = value2;
-  }
+function setValue(input1, input2, set1, set2) {
+  const value1 = document.getElementById(input1).value;
+  const setaddress1 = document.getElementById(set1);
+  setaddress1.innerText = value1;
+  const value2 = document.getElementById(input2).value;
+  const setAddress2 = document.getElementById(set2)
+  setAddress2.innerText = value2;
+}
 
 
 
